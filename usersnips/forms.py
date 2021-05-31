6f872a1b256cdd9f1719aa75ef8a0b3f5dc57b53
Jess webber <jess_webber@outlook.com>
2021-05-31 14:50:23 +0100

    Tweaks to app views

--- usersnips/forms.py
+++ usersnips/forms.py
@@ -2,11 +2,11 @@ from django import forms
 from .models import Snippet
 
 class SnippetForm(forms.ModelForm):
-    topic_description = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Write your topic here'}))
-    snip_description = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Write your text here'}))
+    topic_description = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Date'}))
+    snip_description = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Write your dream here'}))
 
     class Meta:
         model = Snippet
         fields = [
-        'topic_description', 'snip_description', 
+        'topic_description', 'snip_description',
         ]
